efa38991d0b68d7c5fe0efc87b5ff8904a713b21
makiyo-ronkko <makiyo.ronkko@gmail.com>
2020-09-13 12:04:52 +0300

    Initial commit

---
+++ src/jokeList.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class jokeList extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Joke List</h1>
+      </div>
+    );
+  }
+}
+
+export default jokeList;
